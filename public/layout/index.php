48bccf9b08817a364e41da5306a32ecf7372d816
justexpla <Roman.Sudarkin@yandex.ru>
2020-09-22 18:35:57 +0700

    Собрал прототип верстки на бутстрапе

---
+++ public/layout/index.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>LaraBlog</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/offcanvas.css">
+    <!-- Favicons -->
+
+
+    <!-- Custom styles for this template -->
+    <link href="offcanvas.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-purple">
+    <a class="navbar-brand mr-5" href="#">LaraBlog</a>
+    <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Пункт меню 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Пункт меню 2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Пункт меню 3</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Пункт меню 4</a>
+            </li>
+
+            <!--<li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>-->
+        </ul>
+        <ul class="navbar-nav ml-auto text-right">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Пункт меню 6</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Пункт меню 7</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Пункт меню 8</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!--Additional menu-->
+<!--<div class="bg-white shadow-sm">
+    <nav class="nav nav-underline">
+        <a class="nav-link active" href="#">Dashboard</a>
+        <a class="nav-link" href="#">
+            Friends
+            <span class="badge badge-pill bg-light align-text-bottom">27</span>
+        </a>
+        <a class="nav-link" href="#">Explore</a>
+        <a class="nav-link" href="#">Suggestions</a>
+        <a class="nav-link" href="#">Link</a>
+    </nav>
+</div>-->
+
+<main role="main" class="container">
+    <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-danger rounded shadow-sm">
+        <div class="lh-100">
+            <h6 class="mb-0 text-white lh-100">Место под ошибки и информацию</h6>
+        </div>
+    </div>
+
+    <div class="pl-3 pr-3 pt-3 pb-2 bg-white rounded shadow-sm mb-4   <!--mt-3-->">
+        <div class="text-muted pt-1">
+            <div class="mb-3 row">
+                <div class="col-9 d-inline-block">
+                    <h3>Title</h3>
+                </div>
+                <div class="col-3 d-inline-block text-right">
+                    <span class="align-text-bottom">Автор: такой-то</span>
+                    <img src="https://verimedhealthgroup.com/wp-content/uploads/2019/03/profile-img.jpg"
+                         alt="" width="40" height="40"
+                         class="ml-4">
+                </div>
+            </div>
+            <div>
+                <p class="border-bottom border-gray border-top pt-3 pb-3">
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="pl-3 pr-3 pt-3 pb-2 bg-white rounded shadow-sm mb-4">
+        <div class="text-muted pt-1">
+            <div class="mb-3 row">
+                <div class="col-9 d-inline-block">
+                    <h3>Title</h3>
+                </div>
+                <div class="col-3 d-inline-block text-right">
+                    <span class="align-text-bottom">Автор: такой-то</span>
+                    <img src="https://verimedhealthgroup.com/wp-content/uploads/2019/03/profile-img.jpg"
+                         alt="" width="40" height="40"
+                         class="ml-4">
+                </div>
+            </div>
+            <div>
+                <p class="border-bottom border-gray border-top pt-3 pb-3">
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="pl-3 pr-3 pt-3 pb-2 bg-white rounded shadow-sm mb-4">
+        <div class="text-muted pt-1">
+            <div class="mb-3 row">
+                <div class="col-9 d-inline-block">
+                    <h3>Title</h3>
+                </div>
+                <div class="col-3 d-inline-block text-right">
+                    <span class="align-text-bottom">Автор: такой-то</span>
+                    <img src="https://verimedhealthgroup.com/wp-content/uploads/2019/03/profile-img.jpg"
+                         alt="" width="40" height="40"
+                         class="ml-4">
+                </div>
+            </div>
+            <div>
+                <p class="border-bottom border-gray border-top pt-3 pb-3">
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                    tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</body></html>
